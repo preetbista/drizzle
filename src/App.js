d419a3b63eb35cfaec1336846a114bee3299eaa0
Preet Bista <bistapreet4@gmail.com>
2022-12-01 14:44:03 +0545

    Product added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+import SideBar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProductDetails from './pages/ProductDetails';
+
+
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return (
+    <div className='overflow-hidden'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+        </Routes>
+        <SideBar />
+        <Footer />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
