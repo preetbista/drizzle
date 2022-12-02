98992689af4b539e852a657674779560cb2fecb3
Preet Bista <bistapreet4@gmail.com>
2022-12-02 15:45:26 +0545

    Item add, remove, item bag=> item amount display

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,7 +11,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SideBarContext)
-  const { cart } = useContext(CartContext);
+  const { cart, clearCart } = useContext(CartContext);
   return <div className={` ${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all 
   duration-300 z-20 px-4 lg:px-[35px]`} >
     <div className='flex items-center justify-between py-6 border-b' >
@@ -24,7 +24,13 @@ const Sidebar = () => {
       return <CartItem item={item} key={item.id} />;
     })}
     </div>
-  </div>;
+    <div className=' flex flex-col gap-y-3 py-4 mt-4' >
+      <div className='flex w-full justify-between items-center' >
+        <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ 1000</div>
+        <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 justify-center items-center flex text-xl' ><FiTrash2 /></div>
+      </div>
+    </div>
+  </div >;
 };
 
 export default Sidebar;
