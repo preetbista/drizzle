7f5db061dd1fbd4b89f3573f33e0c466edd658e6
Preet Bista <bistapreet4@gmail.com>
2022-12-02 13:21:44 +0545

    cart items increase/decrease

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,7 +11,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SideBarContext)
-  const { } = useContext(CartContext);
+  const { cart } = useContext(CartContext);
   return <div className={` ${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all 
   duration-300 z-20 px-4 lg:px-[35px]`} >
     <div className='flex items-center justify-between py-6 border-b' >
@@ -19,6 +19,10 @@ const Sidebar = () => {
       <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center' >
         <IoMdArrowForward className='text-2xl' />
       </div>
+    </div>
+    <div>{cart.map(item => {
+      return <CartItem item={item} key={item.id} />;
+    })}
     </div>
   </div>;
 };
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,10 +5,26 @@ export const CartContext = createContext()
 const CartProvider = ({ children }) => {
 
   const [cart, setCart] = useState([]);
-  const addToCart = () => {
-    console.log('addet to cart');
-  }
-  return (<CartContext.Provider value={{ addToCart }}>{children}</CartContext.Provider>);
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+    //if cart item is already in the cart
+    if (cartItem) {
+      const newCart = [...cart].map(item => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+  };
+  return (<CartContext.Provider value={{ cart, addToCart }}>{children}</CartContext.Provider>);
 };
 
 export default CartProvider;
