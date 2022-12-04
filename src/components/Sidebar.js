d10d56306dd19a3a01f02bd5bbab413713c58416
Preet Bista <bistapreet4@gmail.com>
2022-12-04 13:12:45 +0545

    added checkout option

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -29,6 +29,8 @@ const Sidebar = () => {
         <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}</div>
         <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 justify-center items-center flex text-xl' ><FiTrash2 /></div>
       </div>
+      <Link to='/' className='bg-gray-200 flex p-2 justify-center items-center text-primary w-full font-medium rounded-lg'>View cart</Link>
+      <Link to='/' className='bg-primary flex p-2 justify-center items-center text-white w-full font-medium rounded-lg'>Checkout</Link>
     </div>
   </div >;
 };
