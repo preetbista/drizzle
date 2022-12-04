6d6214e1bb7801082ff4b72caaaa2f5170b5ded6
Preet Bista <bistapreet4@gmail.com>
2022-12-04 12:28:19 +0545

    total price

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,7 +11,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SideBarContext)
-  const { cart, clearCart } = useContext(CartContext);
+  const { cart, clearCart, total } = useContext(CartContext);
   return <div className={` ${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all 
   duration-300 z-20 px-4 lg:px-[35px]`} >
     <div className='flex items-center justify-between py-6 border-b' >
@@ -20,13 +20,13 @@ const Sidebar = () => {
         <IoMdArrowForward className='text-2xl' />
       </div>
     </div>
-    <div>{cart.map(item => {
+    <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b' >{cart.map(item => {
       return <CartItem item={item} key={item.id} />;
     })}
     </div>
     <div className=' flex flex-col gap-y-3 py-4 mt-4' >
       <div className='flex w-full justify-between items-center' >
-        <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ 1000</div>
+        <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}</div>
         <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 justify-center items-center flex text-xl' ><FiTrash2 /></div>
       </div>
     </div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -6,6 +6,15 @@ const CartProvider = ({ children }) => {
 
   const [cart, setCart] = useState([]);
   const [itemAmount, setItemAmount] = useState(0);
+
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.price * currentItem.amount;
+    }, 0);
+    setTotal(total)
+  });
+
   //update item amount
   useEffect(() => {
     if (cart) {
@@ -81,7 +90,8 @@ const CartProvider = ({ children }) => {
         clearCart,
         increaseAmount,
         decreaseAmount,
-        itemAmount
+        itemAmount,
+        total,
       }}>{children}</CartContext.Provider>);
 };
 
