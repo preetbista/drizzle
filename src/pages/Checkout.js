00380aeff2a1575bc8e32015a7e544f199e5d261
Preet Bista <bistapreet4@gmail.com>
2022-12-06 16:03:48 +0545

    created a card and centered it

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import React from 'react';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+} from "@material-tailwind/react";
+// import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 const Checkout = () => {
     return (
-        <div>
-
-        </div>
-    )
+        <Card className='w-[600px]  text-center justify-center ml-[600px] h-[815px] mt-[100px] mb-[50px]
+        shadow-2xl' >
+            Checkout
+        </Card>
+    );
 }
-
 export default Checkout
